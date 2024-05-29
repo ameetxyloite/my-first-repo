5b4d5f22101fe5968e1cfc349d2199da5bdd8e9d
ameetxyloite <logesh.xyloinc.2023@gmail.com>
2024-05-29 12:01:24 +0530

    Changes on Java Method

---
+++ Methods.java
@@ -0,0 +1,15 @@
+public class Methods{
+    static void myFunction1(){
+        System.out.println("Function 1");        //static method
+    }
+    public void myFunction2()
+    {
+        System.out.println("Function 2");        //instance method
+    }
+public static void main(String[] args) {
+
+    Methods fun1=new Methods();                    //object
+    fun1.myFunction2();
+    myFunction1();
+}
+}
